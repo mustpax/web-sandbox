8ea4e851b87c22147247bc1a3420210b7b1a4710
Mustafa Paksoy <paksoy@gmail.com>
2013-03-07 17:29:19 -0800

    Chart billable vs used hrs.

--- script.js
+++ script.js
@@ -1,27 +1,82 @@
-function data() {
-  var sin = [],
-      cos = [];
+var cols = {"Discounted Instance Hour": true, "Frontend Instance Hours": true};
 
-  for (var i = 0; i < 100; i++) {
-    sin.push({x: i, y: Math.sin(i/10)});
-    cos.push({x: i, y: .5 * Math.cos(i/10)});
-  }
+/* For a given date, get the ISO week number
+ *
+ * Based on information at:
+ *
+ *    http://www.merlyn.demon.co.uk/weekcalc.htm#WNR
+ *
+ * Algorithm is to find nearest thursday, it's year
+ * is the year of the week number. Then get weeks
+ * between that date and the first day of that year.
+ *
+ * Note that dates in one year can be weeks of previous
+ * or next year, overlap is up to 3 days.
+ *
+ * e.g. 2014/12/29 is Monday in week  1 of 2015
+ *      2012/1/1   is Sunday in week 52 of 2011
+ */
+function getWeekNumber(d) {
+    // Copy date so don't modify original
+    d = new Date(d);
+    d.setHours(0,0,0);
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setDate(d.getDate() + 4 - (d.getDay()||7));
+    // Get first day of year
+    var yearStart = new Date(d.getFullYear(),0,1);
+    // Calculate full weeks to nearest Thursday
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7)
+    // Return array of year and week number
+    return d.getFullYear() * 52 + (weekNo % 52);
+}
 
-  return [
-    {
-      values: sin,
-      key: 'Sine Wave',
-      color: '#ff7f0e'
-    },
-    {
-      values: cos,
-      key: 'Cosine Wave',
-      color: '#2ca02c'
+function parseDate(row) {
+    var ret = $.extend({}, row);
+    ret.Date = getWeekNumber(Date.parse(row.Date));
+    return ret;
+}
+
+function filt(row) {
+    return !! cols[row.Name];
+}
+
+function sum(a, b) {
+    return a + b;
+}
+
+function aggr(type) {
+    return function (row, key) {
+        return {
+            x: parseInt(key),
+            y: _.chain(row)
+                .pluck(type)
+                .map(parseFloat)
+                .reduce(sum)
+                .value()
+        };
     }
-  ];
+}
+
+function formatData(d) {
+    var types = ['Used', 'Billable'];
+    $('#raw-data').text(JSON.stringify(d, null, 2));
+
+    return _.map(types, function(type) {
+        return {
+            key: type,
+            values:  _.chain(d)
+                      .filter(filt)
+                      .map(parseDate)
+                      .groupBy('Date')
+                      .map(aggr(type))
+                      .value()
+        };
+    });
 }
 
 function chart(d) {
+    $('#chart-data').text(JSON.stringify(d, null, 2));
     nv.addGraph(function() {
       var chart = nv.models.lineChart();
 
@@ -34,7 +89,7 @@ function chart(d) {
           .tickFormat(d3.format('.02f'));
 
       d3.select('#chart svg')
-          .datum(data())
+          .datum(d)
           .transition().duration(500)
           .call(chart);
 
@@ -47,7 +102,6 @@ function chart(d) {
 $(function() {
     d3.csv('history.csv',
             function(data) {
-                $('#out').text(JSON.stringify(data, null, 2));
-                chart(data);
+                chart(formatData(data));
             });
 });
