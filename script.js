3360767c711b9c74857b63a6d3859c17945c4b55
Mustafa Paksoy <paksoy@gmail.com>
2013-04-02 15:48:29 -0700

    Paste Michelle's code

--- script.js
+++ script.js
@@ -3,46 +3,27 @@ function out(str) {
     $out.text($out.text() + str + '\n');
 }
 
-function data() {
-  var sin = [],
-      cos = [];
-
-  for (var i = 0; i < 100; i++) {
-    sin.push({x: i, y: Math.sin(i/10)});
-    cos.push({x: i, y: .5 * Math.cos(i/10)});
-  }
-
-  return [
-    {
-      values: sin,
-      key: 'Sine Wave',
-      color: '#ff7f0e'
-    },
-    {
-      values: cos,
-      key: 'Cosine Wave',
-      color: '#2ca02c'
-    }
-  ];
-}
-
-nv.addGraph(function() {
-  var chart = nv.models.lineChart();
-
-  chart.xAxis
-      .axisLabel('Time (ms)')
-      .tickFormat(d3.format(',r'));
-
-  chart.yAxis
-      .axisLabel('Voltage (v)')
-      .tickFormat(d3.format('.02f'));
-
-  d3.select('#chart svg')
-      .datum(data())
-      .transition().duration(500)
-      .call(chart);
-
-  nv.utils.windowResize(chart.update);
-
-  return chart;
+// I have to use jQuery instead of $ because of Prototype and other conflicts.
+// I've heard that I can use a no conflict function to take care of that, though?
+jQuery(function() {
+	// Should I use .ajax here? What's the difference between that and .get and .getJSON and making an object with a JSONscriptRequest?
+    jQuery.ajax({
+    	type: "GET",
+    	// Do I put "&callback=" on the end of this? When is that applicable?
+        url: 'http://api.getsatisfaction.com/companies/gsfnmichelle/topics.json?style=update',
+        // JSON or JSONP? I think JSONP takes care of the cross-domain problem by returning the information in a function--is that right?
+        dataType : 'jsonp',
+        // "data" refers to whatever results the call returns, right?
+        success: function(data) {
+        	var updateTitle = data.subject
+        	var updateURL = data.at_sfn
+        	// Where and how do I set a cookie to count visits?
+        	// Is this how I make the returned updateTitle and updateURL show up in the banner?
+        	jQuery(#updates).show().html('<h2 class="loading">Loading a company update . . .</h2>').html('<a href="updateURL" id="theUpdate" src="updateURL", "updateTitle">');
+        }
+        // How do I indicate what should happen with no success/no results?
+        else:
+        	jQuery(#updates).hide();
+    });
 });
+
